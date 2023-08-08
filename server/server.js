d65bbed0c70a05c50f35a66fd64f908f2fa432c4
yusufkarakaya <yusufkarakaya92@gmail.com>
2023-08-07 19:37:55 -0700

    gitignore add new line

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const port = 8000;
 
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cookieParser());
 app.use(express.json(), express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 require('./config/mongoose.config');
 require('dotenv').config();
